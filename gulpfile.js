1d9894dd92fc9bd9c68174f8e4fbd66883eb471f
The Oddball <oddball@oddball.tf>
2018-12-26 20:03:13 -0700

    add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,35 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var csso = require('gulp-csso');
-var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-minify-html');
-var pug = require('gulp-pug');
-var stylus = require('gulp-stylus');
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var csso = require("gulp-csso");
+var uglify = require("gulp-uglify");
+var htmlmin = require("gulp-minify-html");
+var pug = require("gulp-pug");
+var stylus = require("gulp-stylus");
 
-gulp.task('stylus', function () {
-      return gulp.src(['./css/*.styl'])
-            .pipe(stylus())
-            .pipe(csso())
-            .pipe(gulp.dest('./out'))
+gulp.task("stylus", function () {
+	return gulp.src(["./css/*.styl"])
+		.pipe(stylus())
+		.pipe(csso())
+		.pipe(gulp.dest("./out"));
 });
 
-gulp.task('js', function () {
-      return gulp.src(['./js/*.js'])
-            .pipe(concat('main.js'))
-            .pipe(uglify())
-            .pipe(gulp.dest('./out'));
+gulp.task("js", function () {
+	return gulp.src(["./js/*.js"])
+		.pipe(concat("main.js"))
+		.pipe(uglify())
+		.pipe(gulp.dest("./out"));
 });
 
-gulp.task('html', function () {
-      return gulp.src(['index.pug'])
-            .pipe(pug())
-            .pipe(htmlmin())
-            .pipe(gulp.dest('./out'));
+gulp.task("html", function () {
+	return gulp.src(["index.pug"])
+		.pipe(pug())
+		.pipe(htmlmin())
+		.pipe(gulp.dest("./out"));
 });
 
-gulp.task('images', function () {
-      return gulp.src('./img/site/**')
-            .pipe(gulp.dest('./out/img/site'));
+gulp.task("images", function () {
+	return gulp.src("./img/site/**")
+		.pipe(gulp.dest("./out/img/site"));
 });
 
-gulp.task('page', gulp.series('html', 'stylus', 'js'))
-
-gulp.task('default', gulp.parallel('page', 'images'));
+gulp.task("default", gulp.parallel("images", "html", "stylus", "js"));
--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 var bgs = [
-    './img/site/backgrounds/bg.jpg',
-    './img/site/backgrounds/bg1.jpg',
-    './img/site/backgrounds/bg2.jpg',
-    './img/site/backgrounds/bg3.jpg',
-    './img/site/backgrounds/bg4.png',
-    './img/site/backgrounds/bg5.jpg',    
-]
+	"./img/site/backgrounds/bg.jpg",
+	"./img/site/backgrounds/bg1.jpg",
+	"./img/site/backgrounds/bg2.jpg",
+	"./img/site/backgrounds/bg3.jpg",
+	"./img/site/backgrounds/bg4.png",
+	"./img/site/backgrounds/bg5.jpg",    
+];
 
 $(document).ready(function () {
-    $('.main')
-        .css({
-            'opacity': 0,
-            'top': 500
-        })
-        .animate({
-            opacity: 1,
-            top: 0
-        }, {
-            queue: false,
-            duration: 1000
-        });
-    var wallpaper = bgs[Math.floor(Math.random() * bgs.length)];
-    $('body').css("background-image", "url("+wallpaper+")")
+	$(".main")
+		.css({
+			"opacity": 0,
+			"top": 500
+		})
+		.animate({
+			opacity: 1,
+			top: 0
+		}, {
+			queue: false,
+			duration: 1000
+		});
+	var wallpaper = bgs[Math.floor(Math.random() * bgs.length)];
+	$("body").css("background-image", "url("+wallpaper+")");
 });

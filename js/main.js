4fe9bf0b523c45a25086601f62dd8852291ccaf6
The Oddball <oddball@oddball.tf>
2018-12-29 02:49:48 -0700

    Make URL easier to read

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@ function hasWebP() {
 
 function setBackground(webPSupport) {
 	var wallpaper = Math.floor(Math.random() * numberOfBackgrounds+1);
-	$("body").css("background-image", "url(./img/site/backgrounds/"+webPSupport+"/bg"+wallpaper+"."+webPSupport+")");
+	var url = "./img/site/backgrounds/"+webPSupport+"/bg"+wallpaper+"."+webPSupport;
+	$("body").css("background-image", "url("+url+")");
 }
 
 $(document).ready(function () {

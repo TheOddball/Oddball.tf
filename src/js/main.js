041fade07841db42fa07aa82630c3381e2716b96
The Oddball <oddball@oddball.tf>
2019-11-15 15:43:24 -0700

    Change to parcel

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,40 @@
+import $ from 'jQuery'
+window.$ = window.jQuery = $
+
 var numberOfBackgrounds = 8;
 
 function hasWebP() {
-	var rv = $.Deferred(), img = new Image();
-	img.onload = function() { rv.resolve(); };
-	img.onerror = function() { rv.reject(); };
-	img.src = "https://www.gstatic.com/webp/gallery/1.webp";
-	return rv.promise();
+    var rv = $.Deferred(),
+        img = new Image();
+    img.onload = function() { rv.resolve(); };
+    img.onerror = function() { rv.reject(); };
+    img.src = "https://www.gstatic.com/webp/gallery/1.webp";
+    return rv.promise();
 }
 
 function setBackground(webPSupport) {
-	var wallpaper = Math.floor(Math.random() * numberOfBackgrounds+1);
-	var url = "./img/site/backgrounds/"+webPSupport+"/bg"+wallpaper+"."+webPSupport;
-	$("body").css("background-image", "url("+url+")");
+    var wallpaper = Math.floor(Math.random() * numberOfBackgrounds + 1);
+    var url = "./backgrounds/" + webPSupport + "/bg" + wallpaper + "." + webPSupport;
+    $("body").css("background-image", "url(" + url + ")");
 }
 
-$(document).ready(function () {
-	hasWebP().then(function() {
-		setBackground("webp");
-	}, function() {
-		setBackground("jpg");
-	});
+$(document).ready(function() {
+    hasWebP().then(function() {
+        setBackground("webp");
+    }, function() {
+        setBackground("jpg");
+    });
 
-	$(".main")
-		.css({
-			"opacity": 0,
-			"top": 500
-		})
-		.animate({
-			opacity: 1,
-			top: 0
-		}, {
-			queue: false,
-			duration: 1000
-		});
+    $(".main")
+        .css({
+            "opacity": 0,
+            "top": 500
+        })
+        .animate({
+            opacity: 1,
+            top: 0
+        }, {
+            queue: false,
+            duration: 1000
+        });
 });
